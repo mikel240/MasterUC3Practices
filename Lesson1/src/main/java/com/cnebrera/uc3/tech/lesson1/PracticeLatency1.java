7906be4a0df30bf024c396dd7775c62017f457a6
mikel240 <miguel_real_2@hotmail.com>
2020-09-19 20:05:23 +0200

    latency1-finish

--- Lesson1/src/main/java/com/cnebrera/uc3/tech/lesson1/PracticeLatency1.java
+++ Lesson1/src/main/java/com/cnebrera/uc3/tech/lesson1/PracticeLatency1.java
@@ -2,37 +2,44 @@ package com.cnebrera.uc3.tech.lesson1;
 
 import com.cnebrera.uc3.tech.lesson1.simulator.BaseSyncOpSimulator;
 import com.cnebrera.uc3.tech.lesson1.simulator.SyncOpSimulRndPark;
+import org.HdrHistogram.Histogram;
 
+import java.io.PrintStream;
 import java.util.concurrent.TimeUnit;
 
 /**
  * First practice, measure latency on a simple operation
  */
-public class PracticeLatency1
-{
-    /**
-     * Main method to run the practice
-     * @param args command line arument
-     */
-    public static void main(String [] args)
-    {
+public class PracticeLatency1 {
+    final static long COST_TO_MEASURE_NS = TimeUnit.NANOSECONDS.toNanos(40);
+
+    public static void main(String[] args) {
         runCalculations();
     }
 
-    /**
-     * Run the practice calculations
-     */
-    private static void runCalculations()
-    {
+    private static void runCalculations() {
         // Create a random park time simulator
-        BaseSyncOpSimulator syncOpSimulator = new SyncOpSimulRndPark(TimeUnit.NANOSECONDS.toNanos(100), TimeUnit.MICROSECONDS.toNanos(100));
+        BaseSyncOpSimulator syncOpSimulator =
+                new SyncOpSimulRndPark(
+                        TimeUnit.NANOSECONDS.toNanos(100),
+                        TimeUnit.MICROSECONDS.toNanos(100)
+                );
+        // Create the histogram object
+        Histogram histogram = new Histogram(
+                TimeUnit.NANOSECONDS.toNanos(100),
+                TimeUnit.MILLISECONDS.toNanos(2),
+                3
+        );
+        //histogram.setAutoResize(true);
 
-        // Execute the operation lot of times
-        for(int i = 0; i < 100000; i++)
-        {
+        for (int i = 0; i < 100000; i++) {
+            long startTime = System.nanoTime();
             syncOpSimulator.executeOp();
+            long latency = (System.nanoTime() - startTime) - (COST_TO_MEASURE_NS * 2);
+            histogram.recordValue(latency);
         }
 
-        // TODO Show the percentile distribution of the latency calculation of each executeOp call
+        PrintStream histogramLog = new PrintStream(System.out);
+        histogram.outputPercentileDistribution(histogramLog, 1000.0);
     }
 }

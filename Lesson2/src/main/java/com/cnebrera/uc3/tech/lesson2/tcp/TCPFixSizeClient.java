270b31031bee5eabc8d650c1dc617899c720b3fa
mikel240 <miguel_real_2@hotmail.com>
2020-10-06 18:51:33 +0200

    TCPFixSize
    Done

--- Lesson2/src/main/java/com/cnebrera/uc3/tech/lesson2/tcp/TCPFixSizeClient.java
+++ Lesson2/src/main/java/com/cnebrera/uc3/tech/lesson2/tcp/TCPFixSizeClient.java
@@ -4,43 +4,43 @@ import com.cnebrera.uc3.tech.lesson2.util.FixSizeMessage;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.Socket;
 import java.nio.ByteBuffer;
 
 /**
  * TCP client that read messages of fixed size from a server
  */
-public class TCPFixSizeClient
-{
-    public static void main(String argv[]) throws Exception
-    {
-        // TODO 1 Create the client socket
-
-        // TODO 2 Get the input stream
+public class TCPFixSizeClient {
+    public static void main(String argv[]) throws Exception {
+        // Create the client socket
+        final Socket clientSocket = new Socket("localhost", 6789);
+        // Get the input stream
+        final InputStream inputStream = clientSocket.getInputStream();
 
         // Read messages non stop
-        while(true)
-        {
-            // TODO 3 call read message with the input stream
+        while (true) {
+            // Call read message with the input stream
+            readMessage(inputStream);
         }
     }
 
     /**
      * Read a message from the given input Stream
+     *
      * @param inputStream the input stream
      * @throws IOException exception thrown if there is a problem
      */
-    private static void readMessage(final InputStream inputStream) throws IOException
-    {
+    private static void readMessage(final InputStream inputStream) throws IOException {
         // The buffer to read the message bytes
-        final byte [] msgBytes = new byte[FixSizeMessage.MSG_BINARY_SIZE];
+        final byte[] msgBytes = new byte[FixSizeMessage.MSG_BINARY_SIZE];
 
-        // TODO 4 We need to have enough bytes in the stream to contain the whole message
+        // We need to have enough bytes in the stream to contain the whole message
+        while (inputStream.available() < msgBytes.length) ;
 
-        // TODO 5 Read the message bytes
-
+        // Read the message bytes
+        inputStream.read(msgBytes);
         // Create the message
         final FixSizeMessage msg = FixSizeMessage.readMsgFromBinary(ByteBuffer.wrap(msgBytes));
-
         System.out.println("Msg received " + msg);
     }
 }
--- Lesson2/src/main/java/com/cnebrera/uc3/tech/lesson2/tcp/TCPFixSizeServer.java
+++ Lesson2/src/main/java/com/cnebrera/uc3/tech/lesson2/tcp/TCPFixSizeServer.java
@@ -4,53 +4,50 @@ import com.cnebrera.uc3.tech.lesson2.util.FixSizeMessage;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
 
 /**
  * TCP server that send messages of fixed size to a client
  */
-public class TCPFixSizeServer
-{
-    public static void main(String argv[]) throws Exception
-    {
-        // TODO 1 create the welcome socket to accept new connections
-
-        // TODO 2 accept a connection
-
-        // TODO 3 call sendMessagesToClient with the new accepted socket
+public class TCPFixSizeServer {
+    public static void main(String argv[]) throws Exception {
+        // Create a socket
+        final ServerSocket welcomeSocket = new ServerSocket(6789);
+        // Accept connection
+        final Socket connectionSocket = welcomeSocket.accept();
+        // Call sendMessagesToClient
+        sendMessagesToClient(connectionSocket);
     }
 
     /**
      * Send fix size messages to a connected socket client
+     *
      * @param connectionSocket the client connection socket
-     * @throws IOException exception thrown if there is an IO problem
+     * @throws IOException          exception thrown if there is an IO problem
      * @throws InterruptedException exception thrown if interrupted
      */
-    private static void sendMessagesToClient(final Socket connectionSocket) throws IOException, InterruptedException
-    {
+    private static void sendMessagesToClient(final Socket connectionSocket)
+            throws IOException, InterruptedException {
         final OutputStream outputStream = connectionSocket.getOutputStream();
-
         // The buffer to write the
         final ByteBuffer sendBuffer = ByteBuffer.allocate(FixSizeMessage.MSG_BINARY_SIZE);
 
-        while (true)
-        {
+        while (true) {
             // Clear the buffer
             sendBuffer.clear();
-
             // Generate random message
             final FixSizeMessage rndMsg = FixSizeMessage.generateRandomMsg();
-
             // Convert to binary
             rndMsg.toBinary(sendBuffer);
 
-            // TODO 4 Write the contents in the output stream
-
-            // TODO 5 Flush to force the message to be send without batching
+            // Write the contents in the output stream
+            outputStream.write(sendBuffer.array());
+            // Flush to force the message to be send without batching
+            outputStream.flush();
 
             System.out.println("Message sent: " + rndMsg.toString());
-
             // Wait a bit before sending the next message
             Thread.sleep(1000);
         }

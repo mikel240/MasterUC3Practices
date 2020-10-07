060a9ff609403fe21befd1eb2f7a90d0c7b8fe96
mikel240 <miguel_real_2@hotmail.com>
2020-10-07 12:09:34 +0200

    tcp done

--- Lesson2/src/main/java/com/cnebrera/uc3/tech/lesson2/tcp/TCPVarSizeClient.java
+++ Lesson2/src/main/java/com/cnebrera/uc3/tech/lesson2/tcp/TCPVarSizeClient.java
@@ -4,53 +4,52 @@ import com.cnebrera.uc3.tech.lesson2.util.VariableSizeMessage;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.Socket;
 import java.nio.ByteBuffer;
 
 /**
  * TCP client that read messages of variable size from a server
  */
-public class TCPVarSizeClient
-{
-    public static void main(String argv[]) throws Exception
-    {
-        // TODO 1 Create the client socket
+public class TCPVarSizeClient {
+    public static void main(String argv[]) throws Exception {
+        // Create the client socket
+        final Socket clientSocket = new Socket("localhost", 6799);
+        // Get the input stream
+        final InputStream inputStream = clientSocket.getInputStream();
 
-        // TODO 2 Get the input stream
-
-        while(true)
-        {
-            // TODO 3 Call send messages with the input stream
+        while (true) {
+            readMessages(inputStream);
         }
     }
 
     /**
      * Send messages into the input stream
+     *
      * @param inputStream the input stream connected to the socket
      * @throws IOException exception if there is an input output problem
      */
-    private static void readMessages(final InputStream inputStream) throws IOException
-    {
+    private static void readMessages(final InputStream inputStream) throws IOException {
         // The buffer to read the header
-        final byte [] header = new byte[4];
+        final byte[] header = new byte[4];
 
-        // TODO 4 Wait to have at least the header
+        // We need to have enough bytes in the stream to contain the whole message
+        while (inputStream.available() < 4) ;
 
-        // TODO 5 Read the header
-
+        // Read the header
+        inputStream.read(header);
         final int msgSize = ByteBuffer.wrap(header).getInt();
-
         System.out.println("Read MsgSize " + msgSize);
 
         // The buffer to read the message bytes
-        final byte [] msgBytes = new byte[msgSize];
+        final byte[] msgBytes = new byte[msgSize];
 
-        // TODO 6 Wait for the whole message to be ready
-
-        // TODO 7 read the message bytes
+        // Wait for the whole message to be ready
+        while (inputStream.available() < msgBytes.length) ;
+        // Read the message bytes
+        inputStream.read(msgBytes);
 
         // Create the message
         final VariableSizeMessage msg = VariableSizeMessage.readMsgFromBinary(msgSize, ByteBuffer.wrap(msgBytes));
-
         System.out.println("Msg received " + msg);
     }
 }
